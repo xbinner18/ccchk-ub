3846986d0ecf5269fe0d04786331af7b6cec52be
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2022-06-19 09:23:08 +0530

    Dont dump cards that usrs chk

--- ubb/modules/scrape.py
+++ ubb/modules/scrape.py
@@ -8,6 +8,13 @@ async def check_incoming_messages(event):
     entities = event.message.entities
     if event.is_private:
         return
+    is_command = False
+    if entities:
+        for entity in entities:
+            if isinstance(entity, types.MessageEntityBotCommand):
+                is_command = True
+    if is_command:
+        return
     is_cc = False
     if entities:
         for entity in entities:
@@ -17,6 +24,6 @@ async def check_incoming_messages(event):
                 try:
                     await Ubot.send_message(DUMP_ID, event.message.message)
                 except errors.FloodWaitError as e:
+                    print(f'flood wait: {e.seconds}')
                     await asyncio.sleep(e.seconds)
                     await Ubot.send_message(DUMP_ID, event.message.message)
-                    print(f'flood wait: {e.seconds}')
