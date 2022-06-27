039eb13f4a5abef794b5af1f1abb400b7bb6e9c7
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2022-06-27 22:49:07 +0530

    Cleanup: improved ignore bot cmd msgs

--- ubb/modules/scrape.py
+++ ubb/modules/scrape.py
@@ -6,14 +6,9 @@ from ubb import Ubot, DUMP_ID
 @Ubot.on(events.NewMessage())  # pylint:disable=E0602
 async def check_incoming_messages(event):
     entities = event.message.entities
-    if event.is_private:
-        return
-    is_command = False
-    if entities:
-        for entity in entities:
-            if isinstance(entity, types.MessageEntityBotCommand):
-                is_command = True
-    if is_command:
+    prefixes = ['?', '/', '.', '!']
+    m = event.message.message
+    if m.startswith(tuple(prefixes)) or len(m) < 23 or event.is_private:
         return
     is_cc = False
     if entities:
@@ -22,8 +17,9 @@ async def check_incoming_messages(event):
                 is_cc = True
             if is_cc:
                 try:
-                    await Ubot.send_message(DUMP_ID, event.message.message)
+                    await asyncio.sleep(3)
+                    await Ubot.send_message(DUMP_ID, m)
                 except errors.FloodWaitError as e:
                     print(f'flood wait: {e.seconds}')
                     await asyncio.sleep(e.seconds)
-                    await Ubot.send_message(DUMP_ID, event.message.message)
+                    await Ubot.send_message(DUMP_ID, m)
