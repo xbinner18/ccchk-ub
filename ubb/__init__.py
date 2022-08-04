660fb79dae64f73faa99a470645d996ae7e60620
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2022-08-04 11:50:38 +0530

    Update __init__.py

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import yaml
 
 from telethon import TelegramClient
 from telethon.sessions import StringSession
@@ -9,12 +10,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.INFO)
 LOGS = logging.getLogger(__name__)
 
-ENV = bool(os.getenv('ENV', True))
-API_ID = int(os.getenv('API_ID', None))
-API_HASH = os.getenv('API_HASH', None)
-URL = os.getenv('URL', None)
-DUMP_ID = int(os.getenv('DUMP_ID', None))
-STRING_SESSION = os.getenv('STRING_SESSION', None)
+CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
+API_ID = int(os.getenv('API_ID', CONFIG['api_id']))
+API_HASH = os.getenv('API_HASH', CONFIG['api_hash'])
+DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
+STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['string_session'])
 
 Ubot = TelegramClient(StringSession(STRING_SESSION),
                       API_ID,
