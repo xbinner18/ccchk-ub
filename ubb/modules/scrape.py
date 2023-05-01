ee72344012a54468793bd1901833a95011eb4a05
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2023-05-01 12:26:58 +0530

    mmm

--- ubb/modules/scrape.py
+++ ubb/modules/scrape.py
@@ -51,8 +51,8 @@ async def scrapper(event):
                 mm = f'0{mm}'
             if len(yy) == 2:
                 yy = f'20{yy}'
-            if mm+yy < m+y:
-                pass
+            if mm+yy <= m+y:
+                continue
             value = f'{cn}|{mm}|{yy}|{cvv}\n'
             regex = re.compile(r'((?:(^(4|5|6)[0-9]{15,15})|(^3[0-9]{14,14}))\|[0-9]{1,2}\|[0-9]{2,4}\|[0-9]{3,4})')
             if regex.match(value):
