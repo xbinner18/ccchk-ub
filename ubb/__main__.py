04b9394155b850dc40cba7983f90730de88561ff
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2023-06-30 10:42:38 +0530

    Update __main__.py
    Add flask to deploy as webapp

--- ubb/__main__.py
+++ ubb/__main__.py
@@ -4,10 +4,14 @@ import asyncio
 
 from ubb import Ubot
 from ubb.modules import ALL_MODULES
+from flask import request, Response
 
 
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module(f"ubb.modules.{module_name}")
+
+app = Flask(__name__)
+loop = asyncio.get_event_loop()
 
 
 async def main():
@@ -17,7 +21,12 @@ async def main():
               '.help to check command list\n'
               '(Press Ctrl+C to stop this)')
         await Ubot.run_until_disconnected()
- 
+
+
+@app.route("/", methods=['GET'])
+def getMe():
+    return {"MyTelegramAccount": loop.run_until_complete(main())}
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    app.run(host=0.0.0.0, port=5000)
