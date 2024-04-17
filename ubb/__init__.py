661c96209b16580b850d552faccc406cb37e9af1
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2024-04-17 15:25:44 +0530

    Fix heroku deploy get vars correctly

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -11,10 +11,10 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 LOGS = logging.getLogger(__name__)
 
 CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
-API_ID = int(os.getenv('api_id', CONFIG['api_id']))
-API_HASH = os.getenv('api_hash', CONFIG['api_hash'])
-DUMP_ID = os.getenv('dump_id', CONFIG['dump_id'])
-STRING_SESSION = os.getenv('string_session', CONFIG['string_session'])
+API_ID = int(os.getenv('API_ID', CONFIG['api_id']))
+API_HASH = os.getenv('API_HASH', CONFIG['api_hash'])
+DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
+STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['string_session'])
 
 Ubot = TelegramClient(StringSession(STRING_SESSION),
                       API_ID,
