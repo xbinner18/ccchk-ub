e124143a8b5767303105e3303c909a2ab758c77f
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2024-06-11 10:43:56 +0530

    Cleanup
    Remove undefined codes

--- ubb/modules/scrape.py
+++ ubb/modules/scrape.py
@@ -44,8 +44,6 @@ async def xcrapper(event):
                 mm = f'0{mm}'
             if len(yy) == 2:
                 yy = f'20{yy}'
-            if mm+yy <= m+y:
-                continue
             values = f'{cc}|{mm}|{yy}|{cvv}\n'
             regex = re.compile(r'((?:(^(4|5|6)[0-9]{15,15})|(^3[0-9]{14,14}))\|[0-9]{1,2}\|[0-9]{2,4}\|[0-9]{3,4})')
             if regex.match(values):
@@ -98,8 +96,6 @@ async def scrapper(event):
                 mm = f'0{mm}'
             if len(yy) == 2:
                 yy = f'20{yy}'
-            if mm+yy <= m+y:
-                continue
             value = f'{cn}|{mm}|{yy}|{cvv}\n'
             regex = re.compile(r'((?:(^(4|5|6)[0-9]{15,15})|(^3[0-9]{14,14}))\|[0-9]{1,2}\|[0-9]{2,4}\|[0-9]{3,4})')
             if regex.match(value):
